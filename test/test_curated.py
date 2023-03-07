00245eed03bde52049b98f44f18d607a70a0cbb6
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-03-06 21:21:23 -0600

    pre working with test_token_crap

--- test/test_curated.py
+++ test/test_curated.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+import rich
 from pytest import mark
 
 
@@ -7,7 +10,14 @@ def test_curated(grammar, read_curated_file, filename: str):
 
 
 def test_expr(grammar):
-    out = grammar.parse(
-        "(0.016 + Nodes.Length / 40 * 0.016 + Nodes.Length * 0.016)", start="expr"
-    )
-    assert len(out) == 1
+    sample = "0.016 + Nodes.Length / 40 * 0.016 + Nodes.Length * 0.016"
+    # sample = "0.016 + Nodes.Length"
+    sample = "Nodes.Length.Stuff.Such(a,b,x=y).stuff"
+    sample = "Nodes.Length.Stuff.Such().stuff.such.What(a,b)"
+    # sample = "What()"
+    out = grammar.parse(sample, start="test_start", trace=True, parseinfo=False)
+    print("\n")
+    from rich.pretty import pprint
+
+    pprint(sample)
+    pprint(out)
--- test/test_generated.py
+++ test/test_generated.py
@@ -2,30 +2,42 @@ from pprint import pprint
 
 from hypothesis.strategies import characters
 from pytest import mark
-from hypothesis import given, strategies as st
+from hypothesis import given, strategies as st, example
 
 
-@given(number=st.integers())
 @mark.int
+@given(number=st.integers())
+@example(number=0)
+@example(number=-0)
 def test_int_parsing(number: int, grammar):
     out = grammar.parse(str(number), start="number")
-    negorpos = "NEG" if number < 0 else "POS"
     strnum = [str(abs(number))]
-    print(out)
-    assert out == (negorpos, strnum)
+    if number > 0:
+        assert ("POS", strnum) == out
+    elif number < 0:
+        assert ("NEG", strnum) == out
+    elif number == 0:
+        # Zero
+        assert ("POS", strnum) == out
+    elif number == -0:
+        assert ("NEG", strnum) == out
+    print(f"\n{number=}\n{out=}\n{strnum=}")
 
 
-@given(
-    number=st.floats(
-        allow_nan=False, allow_infinity=False, allow_subnormal=False
-    ).filter(lambda x: True if x != 0 and x != -0 else False)
-)
+@given(left=st.integers(), right=st.integers(min_value=1))
 @mark.float
-def test_float_parsing(number: float, grammar):
-    out = grammar.parse(str(number), start="NUMBER")
-    print(f"Orig: {number}")
-    pprint(out)
-    neg = number < 0
+def test_simple_float(left: int, right: int, grammar):
+    num_str = f"{left}.{right}"
+    p, (l, r) = grammar.parse(num_str, start="number")
+    assert int(r) == right
+    if left >= 0:
+        assert int(l) == left
+        assert p == "POS"
+    else:
+        assert int("-" + l) == left
+        assert p == "NEG"
+    print(f'Left: {left}\tParsed Left: {"-" if p == "NEG" else ""}{l}')
+    print(f"Right: {right}\tParsed Right: {r}")
 
 
 @mark.parametrize("delim", ('"', "'"))
@@ -38,11 +50,27 @@ def test_float_parsing(number: float, grammar):
 @mark.string
 def test_string_parsing(gened: str, delim: str, grammar):
     test_str = delim + gened.replace(delim, f"\\{delim}") + delim
-    pprint(test_str)
+
     out = grammar.parse(test_str, start="string")
-    pprint(out)
+    print(f"\n{gened=}\n{out=}")
 
 
+# lowish_int = st.integers(min_value=-1000, max_value=1000)
+lowish_int = st.just(-100)
+operators = "+-/*"
+
+
+@mark.parametrize("op", operators)
+@given(
+    left=lowish_int,
+    right=lowish_int,
+)
 @mark.expr
-def test_parse_math_expr(grammar):
-    pass
+def test_parse_math_expr(op: str, left: int, right: int, grammar):
+    test_expr = f"{left} {op} {right}"
+    out = grammar.parse(
+        test_expr,
+        start="math",
+        trace=True,
+    )
+    print(f"{test_expr}\n{out}")
